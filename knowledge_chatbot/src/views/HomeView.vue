a491bdceeea92e2c291ff87500aac96c6b1c7d80
Mi <phanminhmi@gmail.com>
2026-01-07 00:30:46 +0700

    Fix 401

--- knowledge_chatbot/src/views/HomeView.vue
+++ knowledge_chatbot/src/views/HomeView.vue
@@ -93,7 +93,7 @@ export default {
             method: "POST",
             headers: { 
               "Content-Type": "application/json", 
-               Authorization: 'Bearer ${localStorage.getItem("access_token")}',
+               Authorization: `Bearer ${localStorage.getItem("access_token")}`,
             },
             body: JSON.stringify({ 
               message: text,
@@ -140,7 +140,7 @@ export default {
           `${import.meta.env.VITE_API_BASE_URL || "http://localhost:8000"}/chat/conversations/${id}/messages`,
           {
             headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
+              Authorization: `Bearer ${localStorage.getItem("access_token")}`,
             },
           }
         );
